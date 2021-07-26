fa8e155935252bfc42080eb7bd086d1bfa18d2b4
ajayhf <ajaykumar.r@happyfox.com>
2021-07-26 18:36:40 +0530

    correting the setExpired Boolean

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -12,7 +12,7 @@ const Timer = (props) => {
             if (props.sec === 0) {
                 if (props.min === 0) {
                     props.clearSelected([])
-                    props.setExp(false)
+                    props.setExp(true)
                     clearInterval(myInterval)
                 } else {
                     props.setmin(props.min - 1);
@@ -25,7 +25,7 @@ const Timer = (props) => {
         };
     });
     return (
-        <Text style={{ textAlign: 'center' }}><Image style={{height:15,width:15}} source={ require('../utils/warning.jpeg') }></Image><Text>Your booking will expire in: </Text>{props.min} minutes :{props.sec < 10 ? `0${props.sec}seconds` : props.sec + ' seconds'}</Text>
+        <Text style={{ textAlign: 'center' }}><Image style={{height:15,width:15}} source={require('../utils/warning.jpeg') }></Image><Text>Your booking will expire in: </Text>{props.min} minutes :{props.sec < 10 ? `0${props.sec}seconds` : props.sec + ' seconds'}</Text>
 
     );
 }
